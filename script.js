a6561be1e26975d43d29fcb5597c54414a4fe7a6
RiotHelmet <85107883+RiotHelmet@users.noreply.github.com>
2022-08-28 21:46:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ let ctx = canvas.getContext("2d");
 
 let fps;
 let fpsInfo = document.getElementById("fps");
-let deltaT;
+let deltaT = 1 / 144;
 
 let scale = 100;
-
+let loss = 0.7;
 let mousePos = {
   x: 0,
   y: 0,
 };
+
+let showCircle = true;
 
 let friction = 0.995;
 
@@ -18,66 +20,40 @@ let particles = [];
 
 class particle {
   constructor(x, y, mass) {
-    this.P = {
-      x: 0,
-      y: 0,
-    };
     particles.push(this);
     this.pos = {
       x: x,
       y: y,
     };
-    this.color = getRandomColor();
-    this.oldpos = {
-      x: x,
-      y: y,
-    };
+    this.mass = mass;
     this.velocity = {
       x: 0,
       y: 0,
     };
+
+    this.oldpos = {
+      x: x,
+      y: y,
+    };
+
     this.acceleration = {
       x: 0,
       y: 0,
     };
-    this.mass = mass;
-    this.radius = 0.7 * scale;
+
+    this.color = getRandomColor();
+    this.radius = (mass / 30) * scale;
   }
   draw() {
-    this.P.x = this.mass * this.velocity.x;
-    this.P.y = this.mass * this.velocity.y;
-
-    this.angle = Dir(this.pos, {
-      x: this.pos.x + this.velocity.x,
-      y: this.pos.y + this.velocity.y,
-    });
-
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = "2";
     ctx.fillStyle = `${this.color}`;
     ctx.beginPath();
     ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI);
     ctx.fill();
-
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.arc(this.pos.x, this.pos.y, 2, 0, 2 * Math.PI);
-    ctx.fill();
-
-    ctx.strokeStyle = "blue";
-    ctx.lineWidth = "5";
-    ctx.beginPath();
-    ctx.moveTo(this.pos.x, this.pos.y);
-    ctx.lineTo(
-      this.pos.x +
-        Math.cos(this.angle) *
-          (Math.abs(this.velocity.y) + Math.abs(this.velocity.x)) *
-          5,
-      this.pos.y +
-        Math.sin(this.angle) *
-          (Math.abs(this.velocity.y) + Math.abs(this.velocity.x)) *
-          5
-    );
     ctx.stroke();
   }
+
   accelerate(accX, accY) {
     this.acceleration.x += accX * deltaT * deltaT;
     this.acceleration.y += accY * deltaT * deltaT;
@@ -89,62 +65,126 @@ function clear() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function solveCollisions(Object) {
-  for (let index = 0; index < particles.length; index++) {
-    var Other = particles[index];
-    if (Other !== Object) {
-      if (
-        isTouching(
-          {
-            x: Object.pos.x,
-            y: Object.pos.y,
-          },
-          Other.pos,
-          Other.radius,
-          Object.radius
-        )
-      ) {
-        return [true, Other];
-      }
-    }
-  }
-  return [false, NaN];
-}
-
-function verletCompute() {
+function verletIntegrate() {
   particles.forEach((Object) => {
     Object.velocity.x =
       ((Object.pos.x - Object.oldpos.x) / scale / deltaT) * friction;
     Object.velocity.y =
       ((Object.pos.y - Object.oldpos.y) / scale / deltaT) * friction;
-    // console.log(Object.velocity);
+
     Object.oldpos.x = Object.pos.x;
     Object.oldpos.y = Object.pos.y;
-    if (solveCollisions(Object)[0] == false) {
-      Object.pos.x +=
-        Object.velocity.x * scale * deltaT + Object.acceleration.x * scale;
-      Object.pos.y +=
-        Object.velocity.y * scale * deltaT + Object.acceleration.y * scale;
-    } else {
-      // let Other = solveCollisions(Object)[1];
-      // let collisionAngle = Dir(Object.pos, Other.pos);
-      // console.log(Other.pos);
-      // Object.pos.x -= Math.cos(collisionAngle) * 55;
 
-      // Object.pos.y -= Math.sin(collisionAngle) * 55;
-      // Other.pos.x -= Math.cos(collisionAngle + Math.PI) * 55;
-
-      // Other.pos.y -= Math.sin(collisionAngle + Math.PI) * 55;
-      console.log("yo");
-      Object.pos.x +=
-        Object.velocity.x * scale * deltaT + Object.acceleration.x * scale;
-      Object.pos.y +=
-        Object.velocity.y * scale * deltaT + Object.acceleration.y * scale;
-    }
+    Object.pos.x +=
+      Object.velocity.x * scale * deltaT + Object.acceleration.x * scale;
+    Object.pos.y +=
+      Object.velocity.y * scale * deltaT + Object.acceleration.y * scale;
 
     Object.acceleration.x = 0;
     Object.acceleration.y = 0;
+  });
+}
 
+function solveCollision(Object, Other) {
+  let distance =
+    (Object.pos.x - Other.pos.x) ** 2 + (Object.pos.y - Other.pos.y) ** 2;
+
+  return distance <= (Object.radius + Other.radius) ** 2;
+}
+
+function resolveCollision(Object, Other) {
+  let collisionDistance =
+    Math.abs(dist(Object.pos, Other.pos) - (Object.radius + Other.radius)) / 2;
+
+  const collisionAngle = Dir(Object.pos, Other.pos);
+
+  Object.pos.x += Math.cos(collisionAngle - Math.PI) * collisionDistance;
+  Object.pos.y += Math.sin(collisionAngle - Math.PI) * collisionDistance;
+
+  Other.pos.x += Math.cos(collisionAngle) * collisionDistance;
+  Other.pos.y += Math.sin(collisionAngle) * collisionDistance;
+
+  const m1 = Object.mass;
+  const m2 = Other.mass;
+
+  let vInit1 = Math.sqrt(Object.velocity.x ** 2 + Object.velocity.y ** 2);
+  let vInit2 = Math.sqrt(Other.velocity.x ** 2 + Other.velocity.y ** 2);
+
+  if (Object.velocity.x < 0) {
+    if (Object.velocity.y >= 0) {
+      vInit1 *= -1;
+    }
+  }
+  if (Object.velocity.y < 0) {
+    if (Object.velocity.x >= 0) {
+      vInit1 *= -1;
+    }
+  }
+
+  if (Other.velocity.x < 0) {
+    if (Other.velocity.y >= 0) {
+      vInit2 *= -1;
+    }
+  }
+  if (Other.velocity.y < 0) {
+    if (Other.velocity.x >= 0) {
+      vInit2 *= -1;
+    }
+  }
+
+  let vFin1 =
+    ((m1 - m2) / (m1 + m2)) * vInit1 + ((2 * m2) / (m1 + m2)) * vInit2;
+
+  let vFin2 =
+    ((2 * m1) / (m1 + m2)) * vInit1 + ((m2 - m1) / (m1 + m2)) * vInit2;
+
+  Object.velocity.x = Math.cos(collisionAngle - Math.PI) * vFin1 * loss;
+  Object.velocity.y = Math.sin(collisionAngle - Math.PI) * vFin1 * loss;
+
+  Other.velocity.x = Math.cos(collisionAngle) * vFin2 * loss;
+  Other.velocity.y = Math.sin(collisionAngle) * vFin2 * loss;
+}
+
+function applyConstraints() {
+  if (showCircle == true) {
+    ctx.strokeStyle = "black";
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2, 400, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  particles.forEach((Object) => {
+    for (let i = 0; i < particles.length; i++) {
+      if (particles[i] !== Object) {
+        if (solveCollision(Object, particles[i])) {
+          resolveCollision(Object, particles[i]);
+        } else {
+        }
+      }
+    }
+    if (showCircle == true) {
+      if (
+        dist(Object.pos, { x: canvas.width / 2, y: canvas.height / 2 }) >
+        400 - Object.radius
+      ) {
+        let angle = Dir(Object.pos, {
+          x: canvas.width / 2,
+          y: canvas.height / 2,
+        });
+
+        Object.pos.x +=
+          Math.cos(angle) *
+          (dist(Object.pos, { x: canvas.width / 2, y: canvas.height / 2 }) -
+            400 +
+            Object.radius);
+
+        Object.pos.y +=
+          Math.sin(angle) *
+          (dist(Object.pos, { x: canvas.width / 2, y: canvas.height / 2 }) -
+            400 +
+            Object.radius);
+      }
+    }
     if (Object.pos.x > canvas.width - Object.radius) {
       Object.pos.x = canvas.width - Object.radius;
       Object.oldpos.x = Object.pos.x + Object.velocity.x * deltaT * scale;
@@ -162,94 +202,81 @@ function verletCompute() {
       Object.pos.y = Object.radius;
       Object.oldpos.y = Object.pos.y + Object.velocity.y * deltaT * scale;
     }
-
-    if (
-      dist(Object.pos, { x: canvas.width / 2, y: canvas.height / 2 }) >
-      400 - Object.radius
-    ) {
-      let angle = Dir(Object.pos, {
-        x: canvas.width / 2,
-        y: canvas.height / 2,
-      });
-
-      Object.pos.x +=
-        Math.cos(angle) *
-        (dist(Object.pos, { x: canvas.width / 2, y: canvas.height / 2 }) -
-          400 +
-          Object.radius);
-
-      Object.pos.y +=
-        Math.sin(angle) *
-        (dist(Object.pos, { x: canvas.width / 2, y: canvas.height / 2 }) -
-          400 +
-          Object.radius);
-    }
   });
 }
 
-p1 = new particle(200, 380, 20);
-// p1.acceleration.x += 10 / scale;
-window.addEventListener("mouseup", function (e) {
-  new particle(mousePos.x, mousePos.y, 20);
-  // particles[particles.length - 1].acceleration.x += 10 / scale;
-});
-
 canvas.addEventListener("mousemove", function (e) {
-  mousePos.x = e.offsetX;
-  mousePos.y = e.offsetY;
-  console.log(mousePos);
+  mousePos.x = e.offsetX / 0.75;
+  mousePos.y = e.offsetY / 0.75;
 });
 
-var lastLoop = new Date();
-var timerStart = new Date();
+function drawObjects() {
+  particles.forEach((Object) => {
+    Object.draw();
+    Object.accelerate(0, 10);
+  });
+}
+
+window.addEventListener("keydown", function (e) {
+  if (e.key == "l") {
+    new particle(mousePos.x, mousePos.y, Math.random() * 10 + 4);
+    particles[particles.length - 1].accelerate(1000, 700);
+  }
+
+  if (e.key == "h") {
+    new particle(mousePos.x, mousePos.y, 500);
+    particles[particles.length - 1].accelerate(1000, 700);
+  }
+
+  if (e.key == "k") {
+    showCircle = false;
+  }
+});
 
 function update() {
   requestAnimationFrame(update);
   clear();
-  console.log(p1.P);
-  // Fps counter
-  var thisLoop = new Date();
-  fps = 1000 / (thisLoop - lastLoop);
-  lastLoop = thisLoop;
-  fpsInfo.innerHTML = `Fps: ${Math.round(fps)}`;
-  deltaT = 1 / fps;
+  verletIntegrate();
 
-  verletCompute();
-
-  particles.forEach((Object) => {
-    // console.log(solveCollisions(Object));
-    Object.accelerate(0, 9.82);
-  });
-
-  // console.log(p1.velocity.x / deltaT, p1.velocity.y / deltaT);
-  // Draws all particles
-
-  ctx.fillStyle = "black";
-  ctx.beginPath();
-  ctx.arc(canvas.width / 2, canvas.height / 2, 400, 0, 2 * Math.PI);
-  ctx.fill();
-
-  particles.forEach((Object) => {
-    Object.draw();
-  });
+  applyConstraints();
+  drawObjects();
 }
 
 update();
 
-function isTouching(a, b, c, d) {
-  if (dist(a, b) < c + d) {
-    return true;
-  } else {
-    return false;
-  }
-}
+//
+//
+// Olika funktioner
+//
+//
 
+// Hittar distans mellan två objekt
 function dist(a, b) {
   x1 = a.x;
   y1 = a.y;
   x2 = b.x;
   y2 = b.y;
   return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}
+// Hittar riktning mellan två objekt
+function Dir(origin, other) {
+  x = other.x;
+  y = other.y;
+  if (other.y >= origin.y) {
+    return Math.acos((x - origin.x) / dist(origin, other));
+  } else if (other.y <= origin.y) {
+    return 2 * Math.PI - Math.acos((x - origin.x) / dist(origin, other));
+  }
+}
+
+function degrees_to_radians(degrees) {
+  var pi = Math.PI;
+  return degrees * (pi / 180);
+}
+
+function radians_to_degrees(rad) {
+  var pi = Math.PI;
+  return rad * (180 / pi);
 }
 
 function getRandomColor() {
@@ -259,14 +286,4 @@ function getRandomColor() {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
-}
-
-function Dir(origin, other) {
-  x = other.x;
-  y = other.y;
-  if (other.y >= origin.y) {
-    return Math.acos((x - origin.x) / dist(origin, other));
-  } else if (other.y <= origin.y) {
-    return 2 * Math.PI - Math.acos((x - origin.x) / dist(origin, other));
-  }
 }
